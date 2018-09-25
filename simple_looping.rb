c4071c57ff244c05068e3ac717753fe61e06d370
Jessica Gladstone <jcg321@comcast.net>
2018-09-25 15:40:33 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,9 +5,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
  
   loop do 
-    
-    puts "#{phrase}"
     counter += 
+    puts "#{phrase}"
+    
      if counter >= number_of_times
        break
   end
